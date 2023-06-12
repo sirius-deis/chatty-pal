f80d3bc15f32ef87f0cf2178d6b3b55e99830db0
Sirius Deis <falebsy@gmail.com>
2023-06-12 23:48:25 +0200

    add loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import Loader from './components/loader/loader';
 import './App.css';
 
+import RootLayout from './layouts/rootLayout/rootLayout';
+import AuthLayout from './layouts/authLayout/authLayout';
+
 function App() {
-    return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<Loader />}>
+              <RootLayout />
+            </Suspense>
+          }
+        ></Route>
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<Loader />}>
+              <AuthLayout />
+            </Suspense>
+          }
+        ></Route>
+      </Routes>
+      <Loader />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const StyledLoader = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30rem;
+  height: 30rem;
+  border-radius: 50%;
+  padding: 1px;
+  background: conic-gradient(#0000 10%, var(--main-color)) content-box;
+  -webkit-mask: repeating-conic-gradient(#0000 0deg, #000 1deg 20deg, #0000 21deg 36deg),
+    radial-gradient(farthest-side, #0000 calc(100% - 20px), #000 calc(100% - 8px));
+  -webkit-mask-composite: destination-in;
+  mask-composite: intersect;
+  animation: s4 1s infinite steps(10);
+  @keyframes s4 {
+    to {
+      transform: translate(-50%, -50%) rotate(1turn);
+    }
+  }
+`;
+
+const Loader = () => {
+  return <StyledLoader />;
+};
+
+export default Loader;
