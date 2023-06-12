857315935b0ad76917ef09a386a06693f8aa8db2
Sirius Deis <falebsy@gmail.com>
2023-06-12 23:58:45 +0200

    add useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+const baseURL = '';
+
+const useFetch = (url, options) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(`${baseURL}${url}`, {
+          credentials: 'include',
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          signal: abortController.signal,
+          ...options,
+        });
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+
+        setData(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+    return () => {
+      abortController.abort();
+    };
+  }, [url, options]);
+
+  return [data, isLoading, error];
+};
+
+export default useFetch;
