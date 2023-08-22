1af21ea17bfb5521544dc5b8ec9f40d730312a60
Sirius Deis <falebsy@gmail.com>
2023-08-22 22:11:16 +0200

    add tests for burger component

---
+++ src/components/burger/burger.test.js
@@ -0,0 +1,16 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import Burger from './burger';
+
+describe('Burger component', () => {
+  it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<Burger onClick={fn}></Burger>);
+    expect(container).toMatchSnapshot();
+  });
+  it('should should fire a click event', () => {
+    const fn = jest.fn();
+    render(<Burger onClick={fn}></Burger>);
+    fireEvent.click(screen.getByRole('button'));
+    expect(fn).toHaveBeenCalled();
+  });
+});
