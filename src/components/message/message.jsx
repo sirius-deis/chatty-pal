e7d5876878c02a15230220058ce48af2e49865ec
Sirius Deis <falebsy@gmail.com>
2023-09-09 23:10:03 +0200

    add styles for the last message in a message group

--- src/components/message/message.jsx
+++ src/components/message/message.jsx
@@ -1,13 +1,28 @@
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { BsCheck2All } from 'react-icons/bs';
 
 const StyledMessage = styled.div`
   max-width: 55%;
+  position: relative;
   padding: 0.7rem 1.4rem;
   font-size: 1.7rem;
   background-color: var(--bg-color-lighter);
   border-radius: 10px;
-  &.last {
+  &.last::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 10px;
+    height: 10px;
+    background-color: var(--bg-color-lighter);
+    z-index: -1;
+  }
+  &.last:not(.own)::before {
+    left: 0;
+  }
+  &.own.last::before {
+    right: 0;
   }
 `;
 
@@ -27,9 +42,16 @@ const StyledInfo = styled.div`
 `;
 
 const Message = ({ children, isLast, isOwn }) => {
+  const dispatch = useDispatch();
   const time = children.createdAt.match(/T(\d{1,2}:\d{1,2}:\d{1,2})\./)[1];
+
+  const onMessageClickHandler = () => {};
+
   return (
-    <StyledMessage className={`${isLast ? 'last' : ''} ${isOwn ? 'own' : ''}`}>
+    <StyledMessage
+      className={`${isLast ? 'last' : ''} ${isOwn ? 'own' : ''}`}
+      onDoubleClick={onMessageClickHandler}
+    >
       {children.message}
       <StyledInfo>
         {time}
