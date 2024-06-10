649284ffc2399e099119f70b31a8004da7c9c3a2
Sirius Deis <falebsy@gmail.com>
2024-06-10 05:13:44 +0200

    add submitting a form in profile component

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,8 +3,10 @@ import Panel from "../panel/panel";
 import Input from "../input/input";
 import Button from "../button/button";
 import Row from "../row/row";
+import { useDispatch, useSelector } from "react-redux";
 import Image from "../../assets/images/no-camera.png";
 import { useState } from "react";
+import { updateUserInfo } from "../../store/user/user.actions";
 
 export const StyledProfile = styled.div``;
 
@@ -14,6 +16,12 @@ const Profile = () => {
   const [name, setName] = useState(user.name);
   const [email, setEmail] = useState(user.email);
   const [bio, setBio] = useState(user.bio);
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(updateUserInfo({ name, email, bio, profilePicture: photo }));
+  };
 
   const onChange = (fn, val) => {
     fn(val);
@@ -21,34 +29,36 @@ const Profile = () => {
 
   return (
     <Panel>
-      <Row>
-        <h2>Profile</h2>
-      </Row>
-      <img src={photo ? photo : Image} alt="avatar" />
-      <h2>Change profile picture</h2>
-      <Input
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => onChange(setName, e.target.Value)}
-      />
-      <Input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => onChange(setEmail, e.target.Value)}
-      />
-      <Input
-        type="text"
-        name="bio"
-        placeholder="Bio"
-        value={bio}
-        onChange={(e) => onChange(setBio, e.target.Value)}
-      />
+      <form onSubmit={onSubmit}>
+        <Row>
+          <h2>Profile</h2>
+        </Row>
+        <img src={photo ? photo : Image} alt="avatar" />
+        <h2>Change profile picture</h2>
+        <Input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => onChange(setName, e.target.Value)}
+        />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => onChange(setEmail, e.target.Value)}
+        />
+        <Input
+          type="text"
+          name="bio"
+          placeholder="Bio"
+          value={bio}
+          onChange={(e) => onChange(setBio, e.target.Value)}
+        />
 
-      <Button>Save changes</Button>
+        <Button>Save changes</Button>
+      </form>
     </Panel>
   );
 };
