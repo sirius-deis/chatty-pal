818af4362d214eecc492098dba856923a5b79016
Sirius Deis <falebsy@gmail.com>
2024-06-29 23:54:07 +0200

    add styles to a filePicker component

--- src/components/filePicker/filePicker.styles.js
+++ src/components/filePicker/filePicker.styles.js
@@ -1,9 +1,21 @@
 import styled from "styled-components";
 
-const StyledFilePicker = styled.label``;
+const StyledFilePicker = styled.label`
+  position: relative;
+  cursor: pointer;
+  border: 1px solid var(--text);
+  border-radius: 5px;
+`;
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  visibility: hidden;
+`;
 
-const StyledIconWrapper = styled.image``;
+const StyledIconWrapper = styled.div`
+  max-width: ${(props) => `${props.size}rem`};
+`;
 
 export { StyledFilePicker, StyledInput, StyledIconWrapper };
