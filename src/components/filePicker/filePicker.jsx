3745ff7949b95030a4e0312a69595e09d3a95934
Sirius Deis <falebsy@gmail.com>
2024-06-30 20:56:39 +0200

    add passing onChange function, handling it and passing it to onChange function if the length passes signleFile bool value

--- src/components/filePicker/filePicker.jsx
+++ src/components/filePicker/filePicker.jsx
@@ -4,11 +4,26 @@ import {
   StyledInput,
   StyledIconWrapper,
 } from "./filePicker.styles";
+import { useState } from "react";
 
-const FilePicker = ({ icon, size = 2, ...rest }) => {
+const FilePicker = ({ icon, size = 2, onChange, singleFile, ...rest }) => {
+  const [isError, setIsError] = useState(false);
+  const onChangeHandler = (event) => {
+    if (singleFile && event.target.files.length > 1) {
+      setIsError(true);
+      return;
+    }
+    onChange(event.target.files);
+  };
+
   return (
-    <StyledFilePicker>
-      <StyledInput type="file" {...rest} data-testid="file" />
+    <StyledFilePicker className={isError ? "error" : ""}>
+      <StyledInput
+        type="file"
+        {...rest}
+        data-testid="file"
+        onChange={onChangeHandler}
+      />
       <StyledIconWrapper size={size} data-testid="file-icon">
         {icon}
       </StyledIconWrapper>
@@ -20,6 +35,7 @@ FilePicker.propTypes = {
   icon: PropTypes.node,
   size: PropTypes.number,
   onChange: PropTypes.func,
+  singleFile: PropTypes.bool,
 };
 
 export default FilePicker;
