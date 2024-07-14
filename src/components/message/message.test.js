0dc60482779d4829928fe1da3e7e55b38ba4c3b9
Sirius Deis <falebsy@gmail.com>
2024-07-14 23:14:05 +0200

    add a test for checking timestamp in a message component

--- src/components/message/message.test.js
+++ src/components/message/message.test.js
@@ -1,21 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import Message from "./message";
 
+const time = new Date("2024-07-17T21:08:30").toISOString();
+
+const message = {
+  message: "Test Message",
+  createdAt: time,
+};
+
 describe("Message component", () => {
   it("should match snapshot", () => {
-    const { container } = render(
-      <Message>
-        {{ message: "Test Message", createdAt: new Date().toISOString() }}
-      </Message>
-    );
+    const { container } = render(<Message>{message}</Message>);
     expect(container).toMatchSnapshot();
   });
   it("should contain provided text", () => {
-    render(
-      <Message>
-        {{ message: "Test Message", createdAt: new Date().toISOString() }}
-      </Message>
-    );
+    render(<Message>{message}</Message>);
     expect(screen.getByText(/Test Message/)).toBeInTheDocument();
+  });
+  it("should have a timestamp", () => {
+    render(<Message>{message}</Message>);
+    expect(screen.getByText(/[0-9]{2}:[0-9]{2}:[0-9]{2}/)).toBeInTheDocument();
   });
 });
