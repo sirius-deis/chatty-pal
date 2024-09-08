ccb50de1c66950351c2d29aef2226fabd4c0499c
Sirius Deis <falebsy@gmail.com>
2024-09-08 19:26:58 +0200

    add a html for a contacts component

---
+++ src/components/contacts/contacts.jsx
@@ -0,0 +1,15 @@
+import List from "../list/list";
+import Modal from "../modal/modal";
+
+const Contacts = () => {
+  return <Modal
+    withCloseBtn
+
+  >
+    <List>
+      
+    </List>
+  </Modal>
+}
+
+export default Contacts;
--- src/components/error/error.styles.js
+++ src/components/error/error.styles.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const StyledError = styled.div`
   width: 100%;
+  height: 100%;
   text-align: center;
   padding: 0.3rem 10rem;
   border-radius: 15px;
