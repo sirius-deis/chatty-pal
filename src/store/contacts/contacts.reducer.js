e118c84e76fb9329cb61e2fc43df360f08218617
Sirius Deis <falebsy@gmail.com>
2024-09-08 19:47:02 +0200

    add cases for starting a failure actions to a contacts reducer

--- src/store/contacts/contacts.reducer.js
+++ src/store/contacts/contacts.reducer.js
@@ -6,6 +6,22 @@ const INITIAL_STATE = {
 
 const contactsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ContactsActionTypes.FETCH_CONTACTS_START:
+    case ContactsActionTypes.ADD_CONTACT_START:
+    case ContactsActionTypes.DELETE_CONTACT_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: null
+      }
+      case ContactsActionTypes.FETCH_CONTACTS_FAILURE:
+    case ContactsActionTypes.ADD_CONTACT_FAILURE:
+    case ContactsActionTypes.DELETE_CONTACT_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      }
     default:
       return state;
   }
