00fc88d1810022e2e1c1862cb60b21f852747dde
Sirius Deis <falebsy@gmail.com>
2024-09-09 20:12:14 +0200

    add an action for adding a contact

--- src/store/contacts/contacts.actions.js
+++ src/store/contacts/contacts.actions.js
@@ -9,3 +9,17 @@ export const fetchContacts = () => async (dispatch) => {
     dispatch({ type: ContactsActionTypes.FETCH_CONTACTS_FAILURE, payload: error });
   }
 }
+
+export const addContact = (contact) => async (dispatch) => {
+  dispatch({ type: ContactsActionTypes.ADD_CONTACT_START });
+  try {
+    const data = await fetchData("contacts", {
+      method: "POST",
+      body: JSON.stringify(contact),
+    });
+    dispatch({ type: ContactsActionTypes.ADD_CONTACT_SUCCESS, payload: data });
+  } catch(error) {
+    dispatch({ type: ContactsActionTypes.ADD_CONTACT_FAILURE, payload: error });
+  }
+}
+
