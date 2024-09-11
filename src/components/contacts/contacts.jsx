30966521a8f36fc50ba63c0bd1d7b433ea85851b
Sirius Deis <falebsy@gmail.com>
2024-09-11 20:40:28 +0200

    add dispatching fetchContacts in contacts component

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -2,20 +2,21 @@ import { useDispatch, useSelector } from "react-redux";
 import List from "../list/list";
 import Modal from "../modal/modal";
 import { useEffect } from "react";
+import { fetchContacts } from "../../store/contacts/contacts.actions";
 
 const Contacts = () => {
   const { contacts } = useSelector(state => state.contacts);
   const dispatch = useDispatch();
 
   useEffect(() => {
-      // dispatch()
+      dispatch(fetchContacts())
   }, [dispatch]);
 
   return <Modal
     withCloseBtn
   >
     <List>
-
+      {contacts.map(contact => <div key={contact._id}>{contact.name}</div>)}
     </List>
   </Modal>
 }
