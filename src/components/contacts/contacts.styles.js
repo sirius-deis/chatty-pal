e9e9c7601958ae884ee90e1f40b3a1eba0a1055b
Sirius Deis <falebsy@gmail.com>
2024-09-13 23:38:28 +0200

    add styles to a contacts component

--- src/components/contacts/contacts.styles.js
+++ src/components/contacts/contacts.styles.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
-export const StyledContactContainer = styled.div``;
+export const StyledContactContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
 
-export const StyledContactPhoto = styled.div``;
+export const StyledContactPhoto = styled.div`
+  flex-basis: 25%;
+  img {
+    width: 100%;
+    border-radius: 50%;
+  }
+`;
 
-export const StyledContactInfo = styled.div``; 
+export const StyledContactInfo = styled.div`
+  flex-basis: 50%;
+`; 
