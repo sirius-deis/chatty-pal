6259d282a28cda5ab6dc2fba8ca816a21e157b61
Sirius Deis <falebsy@gmail.com>
2024-09-14 23:45:17 +0200

    add a test for a messageInfo component

---
+++ src/components/messageInfo/messageInfo.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import MessageInfo from "./messageInfo";
+
+const timestamp = "2024-09-13T12:04:25."
+
+describe("MessageInfo component", () => {
+  it("should match snapshot", () => {
+    const { container } = render(<MessageInfo createdAt={timestamp} isOwn isRead={false}/>);
+    expect(container).toMatchSnapshot();
+  });
+})
