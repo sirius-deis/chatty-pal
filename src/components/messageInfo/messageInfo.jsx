cdf67a9300a95b094a66c4a7a8b67ddd2df5285f
Sirius Deis <falebsy@gmail.com>
2024-09-14 23:35:49 +0200

    add a component for rendering a messageInfo

---
+++ src/components/messageInfo/messageInfo.jsx
@@ -0,0 +1,16 @@
+import { BsCheck2All, BsCheck } from "react-icons/bs";
+
+const MessageInfo = ({isOwn, isRead, createdAt}) => {
+  const time = createdAt.match(/T(\d{1,2}:\d{1,2}:\d{1,2})\./)[1];
+    <StyledMessageInfo>
+      {time}
+      {isOwn && !isRead && (
+        <BsCheck style={{ color: "var(--text)" }} />
+      )}
+      {isOwn && isRead && (
+        <BsCheck2All style={{ color: "var(--primary)" }} />
+      )}
+    </StyledMessageInfo>
+}
+
+export default MessageInfo;
