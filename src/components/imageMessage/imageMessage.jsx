634c401c53f983e0fbfc3a7423cb9cd332e763e7
Sirius Deis <falebsy@gmail.com>
2024-09-15 23:18:56 +0200

    add a messageInfo component to a imageMessage component

--- src/components/imageMessage/imageMessage.jsx
+++ src/components/imageMessage/imageMessage.jsx
@@ -1,8 +1,17 @@
+import PropTypes from "propTypes";
+import MessageInfo from "../messageInfo/messageInfo";
+
 const ImageMessage = ({message, isOwn}) => {
   return <div>
     <img src={message.imageURL} alt={message.imageURL} />
     <p>{message.message}</p>
+    <MessageInfo createdAt={message.createdAt} isOwn={isOwn} isRead={message.isRead} />
   </div>
+}
+
+ImageMessage.propTypes = {
+  message: PropTypes.object.isRequired,
+  isOwn: PropTypes.bool.isRequired,
 }
 
 export default ImageMessage;
