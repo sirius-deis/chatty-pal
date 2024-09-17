5c9a0b508228b1d84c2583f7c19f6cc11d9c3f99
Sirius Deis <falebsy@gmail.com>
2024-09-17 23:15:44 +0200

    add styles for an imageMessage component

--- src/components/imageMessage/imageMessage.styles.js
+++ src/components/imageMessage/imageMessage.styles.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
-export const StyledImageMessage = styled.div``;
+export const StyledImageMessage = styled.div`
+  max-width: 55%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  gap: 0.1rem;
+  &.last::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 10px;
+    height: 10px;
+    background-color: var(--warning-300);
+    z-index: -1;
+  }
+  &.last:not(.own)::before {
+    left: 0;
+  }
+  &.own.last::before {
+    right: 0;
+  }
+  & img {
+    max-width: 100%;
+  }
+`;
 
-export const StyledTextMessage = styled.p``;
+export const StyledTextMessage = styled.p`
+  font-size: 1.7rem;
+  background-color: var(--warning-300);
+  border-radius: 10px;`
+  ;
