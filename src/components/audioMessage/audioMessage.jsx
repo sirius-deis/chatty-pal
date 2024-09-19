ca231ae22b1a7849597eaeb996470e6dc3d930ee
Sirius Deis <falebsy@gmail.com>
2024-09-19 22:05:01 +0200

    add visualization for audio messages

--- src/components/audioMessage/audioMessage.jsx
+++ src/components/audioMessage/audioMessage.jsx
@@ -1,16 +1,27 @@
 import { FaPlay } from "react-icons/fa6";
 import { FaPause } from "react-icons/fa6";
+import { AudioVisualizer } from 'react-audio-visualize';
 import Button from "../button/button";
 import {StyledAudioMessage, StyledAudio} from './audioMessage.styles'
 import { useState } from "react";
 
 const AudioMessage = ({message}) => {
   const [listening, setIsListening] = useState(false)
+  const visualizerRef = useRef(null)
   return <StyledAudioMessage>
     <StyledAudio src={message.src}></StyledAudio>
     <Button type="empty" onClick={() => setIsListening(!listening)}>
       {listening? <FaPause /> : <FaPlay />}
     </Button>
+    {message.src && <AudioVisualizer
+          ref={visualizerRef}
+          blob={blob}
+          width={500}
+          height={75}
+          barWidth={1}
+          gap={0}
+          barColor={'#f76565'}
+        />}
   </StyledAudioMessage>
 }
 
