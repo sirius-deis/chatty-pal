ac1068ff52162b72cfe145b671f521b184b8aed7
Sirius Deis <falebsy@gmail.com>
2024-09-25 21:31:27 +0200

    add html for a videMessage component

--- src/components/videoMessage/videoMessage.jsx
+++ src/components/videoMessage/videoMessage.jsx
@@ -1,5 +1,15 @@
-const VideoMessage = () => {
+import PropTypes from "prop-types";
 
+const VideoMessage = ({message, isOwn}) => {
+  return <StyledVideoMessage>
+    <StyledVideo src={message.src}></StyledVideo>
+        <MessageInfo createdAt={message.createdAt} isRead={message.isRead} isOwn={isOwn}/>
+  </StyledVideoMessage>
+}
+
+VideoMessage.propTypes = {
+  message: PropTypes.object.isRequired,
+  isOwn: PropTypes.bool,
 }
 
 export default VideoMessage;
