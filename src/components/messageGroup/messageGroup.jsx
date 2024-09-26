1f5423d742efcd0e70759584e9fe8c2dacedb60d
Sirius Deis <falebsy@gmail.com>
2024-09-26 21:19:38 +0200

    add cases in a messageGroup component

--- src/components/messageGroup/messageGroup.jsx
+++ src/components/messageGroup/messageGroup.jsx
@@ -2,6 +2,8 @@ import Message from "../message/message";
 import React from "react";
 import { StyledMessageGroup } from "./messageGroup.styles";
 import ImageMessage from "../imageMessage/imageMessage";
+import AudioMessage from "../audioMessage/audioMessage";
+import VideoMessage from "../videoMessage/videoMessage";
 
 //TODO: Add different types of messages
 const MessageGroup = ({ messages, userId }) => {
@@ -20,6 +22,12 @@ const MessageGroup = ({ messages, userId }) => {
             break;
           case "image":
             Component = ImageMessage
+            break;
+          case 'audio':
+            Component = AudioMessage
+            break;
+          case 'video':
+            Component = VideoMessage
             break;
           default:
             Component = Message;
